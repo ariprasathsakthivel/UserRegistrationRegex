60e4623daeb1f8d3662ac1d6b44b6870f1c8674a
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-07-24 22:35:55 +0530

    [Ariprasath] added: testcases for testing the email validator code

--- Program.cs
+++ Program.cs
@@ -7,21 +7,48 @@ namespace UserRegistrationRegex
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Enter your First name to check");
-            //string firstName = Console.ReadLine();
-            //Console.WriteLine(NameCheck(firstName));
-            //Console.WriteLine("Enter your First name to check");
-            //string lastName = Console.ReadLine();
-            //Console.WriteLine(NameCheck(lastName));
-            //Console.WriteLine("Enter your Email ID");
-            //string eMail = Console.ReadLine();
-            //Console.WriteLine(EmailCheck(eMail));
-            //Console.WriteLine("Enter your Phone number to check");
-            //string number = Console.ReadLine();
-            //Console.WriteLine(PhoneNumberCheck(number));
+            #region For checking the first name
+            Console.WriteLine("Enter your First name to check");
+            string firstName = Console.ReadLine();
+            Console.WriteLine(NameCheck(firstName));
+            #endregion
+
+            #region For checking Last Name
+            Console.WriteLine("Enter your Last name to check");
+            string lastName = Console.ReadLine();
+            Console.WriteLine(NameCheck(lastName));
+            #endregion
+
+            #region For checking mail using testcases added
+            string[] testMails = new string[]
+            {
+                "abc@yahoo.com",
+                "abc-100@yahoo.com",
+                "abc.100@yahoo.com",
+                "abc111@yahoo.com",
+                "abc-100@yahoo.com",
+                "abc.100@yahoo.com",
+                "abc@1.com",
+                "abc@gmail.com.com",
+                "abc+100@gmail.com"
+            };
+            foreach (string mail in testMails)
+            {
+                Console.WriteLine("{0}-->{1}", mail, EmailCheck(mail));
+            }
+            #endregion
+
+            #region For checking the Phone number
+            Console.WriteLine("Enter your Phone number to check");
+            string number = Console.ReadLine();
+            Console.WriteLine(PhoneNumberCheck(number));
+            #endregion
+
+            #region For checking the password
             Console.WriteLine("Enter the password to check");
             string password = Console.ReadLine();
             Console.WriteLine(PasswordCheck(password));
+            #endregion
         }
         public static bool NameCheck(string name)
         {
@@ -30,7 +57,7 @@ namespace UserRegistrationRegex
         }
         public static bool EmailCheck(string eMail)
         {
-            string pattern = "^[a-z]{2,}[.]?[a-z]*[@][a-z]{2,}[.][a-z]{2,3}([.]{0,1}[a-z]{0,2})?$";
+            string pattern = "^[a-z]{2,}[-.+]?[a-z]*[0-9]*[@][a-z]*[0-9]*[.][a-z]{2,3}[.]{0,1}[a-z]{0,3}$";
             return Regex.IsMatch(eMail, pattern);
         }
         public static bool PhoneNumberCheck(string number)
@@ -40,7 +67,7 @@ namespace UserRegistrationRegex
         }
         public static bool PasswordCheck(string password)
         {
-            string pattern = "^.*[!@#$%^&*,.-_] [0-9]*$";
+            string pattern = "^.*[!@#$%^&*,.-_][0-9]*$";
             return Regex.IsMatch(password, pattern);
         }
     }
