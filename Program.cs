fc15d2f2de03535ec6be896b61d44bc82f0aa897
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-07-24 16:03:01 +0530

    [Ariprasath] added: Check the provided phone number is in the format 91 XXXXXXXXXX

--- Program.cs
+++ Program.cs
@@ -13,9 +13,12 @@ namespace UserRegistrationRegex
             //Console.WriteLine("Enter your First name to check");
             //string lastName = Console.ReadLine();
             //Console.WriteLine(NameCheck(lastName));
-            Console.WriteLine("Enter your Email ID");
-            string eMail = Console.ReadLine();
-            Console.WriteLine(EmailCheck(eMail));
+            //Console.WriteLine("Enter your Email ID");
+            //string eMail = Console.ReadLine();
+            //Console.WriteLine(EmailCheck(eMail));
+            Console.WriteLine("Enter your Phone number to check");
+            string number = Console.ReadLine();
+            Console.WriteLine(PhoneNumberCheck(number));
         }
         public static bool NameCheck(string name)
         {
@@ -24,8 +27,13 @@ namespace UserRegistrationRegex
         }
         public static bool EmailCheck(string eMail)
         {
-            string pattern = "^[a-z]{2,}[.]?[a-z    ]*[@][a-z]{2,}[.][a-z]{2,3}([.]{0,1}[a-z]{0,2})?";
+            string pattern = "^[a-z]{2,}[.]?[a-z]*[@][a-z]{2,}[.][a-z]{2,3}([.]{0,1}[a-z]{0,2})?$";
             return Regex.IsMatch(eMail, pattern);
+        }
+        public static bool PhoneNumberCheck(string number)
+        {
+            string pattern = "^[9][1][ ][6-9][0-9]{9}$";
+            return Regex.IsMatch(number, pattern);
         }
     }
 }
