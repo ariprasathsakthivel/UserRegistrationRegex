566ac2cb7f197664be5b575af76eef1d20d154e8
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-07-24 15:49:12 +0530

    [Ariprasath] added: check whether the email provided is in the correct format or not.

--- Program.cs
+++ Program.cs
@@ -7,17 +7,25 @@ namespace UserRegistrationRegex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your First name to check");
-            string firstName = Console.ReadLine();
-            Console.WriteLine(NameCheck(firstName));
-            Console.WriteLine("Enter your First name to check");
-            string lastName = Console.ReadLine();
-            Console.WriteLine(NameCheck(lastName));
+            //Console.WriteLine("Enter your First name to check");
+            //string firstName = Console.ReadLine();
+            //Console.WriteLine(NameCheck(firstName));
+            //Console.WriteLine("Enter your First name to check");
+            //string lastName = Console.ReadLine();
+            //Console.WriteLine(NameCheck(lastName));
+            Console.WriteLine("Enter your Email ID");
+            string eMail = Console.ReadLine();
+            Console.WriteLine(EmailCheck(eMail));
         }
         public static bool NameCheck(string name)
         {
             string pattern = "^[A-Z]{1}[a-z]{2,}$";
             return Regex.IsMatch(name, pattern);
+        }
+        public static bool EmailCheck(string eMail)
+        {
+            string pattern = "^[a-z]{2,}[.]?[a-z    ]*[@][a-z]{2,}[.][a-z]{2,3}([.]{0,1}[a-z]{0,2})?";
+            return Regex.IsMatch(eMail, pattern);
         }
     }
 }
